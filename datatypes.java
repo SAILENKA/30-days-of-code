7e64c381145321a2d176187fe5f03b623fb000b2
SAILENKA <87110724+SAILENKA@users.noreply.github.com>
2021-07-10 12:43:17 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+class Datatypes
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=4+sc.nextInt();
+		double b=4+sc.nextDouble();
+		sc.nextLine();
+		String str=sc.nextLine();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("Hacker "+str);
+	}
+}
